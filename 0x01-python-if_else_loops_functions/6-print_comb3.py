4804a48ba8ede1b76f68abc7fc761ad58be75e07
peytonbrsmith <peytonbrsmith@gmail.com>
2020-12-08 14:03:00 -0600

    changed print syntax

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(10):
         if j > i:
             if (i != 8 or j != 9):
-                print(i, j, sep='', end=', ')
+                print("{}{}".format(i, j), end=', ')
             else:
-                print(i, j, sep='')
+                print("{}{}".format(i, j))
