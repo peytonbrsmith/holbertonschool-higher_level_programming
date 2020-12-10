f51b8d30fd8c09b079b7ac4ca82a4ba11b365093
peytonbrsmith <peytonbrsmith@gmail.com>
2020-12-10 14:07:55 -0600

    added check for singular arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     argc = len(argv)
-    print("{} arguments{}".format(argc - 1, ":" if argc > 1 else "."))
+    if argc != 2:
+        print("{} arguments{}".format(argc - 1, ":" if argc > 1 else "."))
+    else:
+        print("1 argument:")
     for i in range(1, argc):
         print("{}: {}".format(i, argv[i]))
