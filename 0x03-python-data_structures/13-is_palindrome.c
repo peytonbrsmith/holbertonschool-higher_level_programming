614299516decbc431a576702c8bf2e074bf485de
peytonbrsmith <peytonbrsmith@gmail.com>
2020-12-15 23:47:29 +0000

    added task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * is_palindrome - detects palindromic singly linked lists
+ * @head: head of the list
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *cmpcpy = *head;
+	listint_t *cpy = *head;
+	unsigned int len, i;
+
+	for (len = 0; cmpcpy->next != NULL; len++)
+		cmpcpy = cmpcpy->next;
+	while (len > 0)
+	{
+		if (cpy->n == cmpcpy->n)
+		{
+			len--;
+			cpy = cpy->next;
+			cmpcpy = *head;
+			for (i = len; i > 0; i--)
+				cmpcpy = cmpcpy->next;
+		}
+		else
+			break;
+	}
+	if (cpy->n == cmpcpy->n)
+		return (1);
+	return (0);
+}
