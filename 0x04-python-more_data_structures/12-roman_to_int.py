627dfbc37890d44bc4492b305cbb9aeede7dd0f7
peytonbrsmith <peytonbrsmith@gmail.com>
2020-12-17 16:57:05 -0600

    added if check for existence

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_val = 0
-    for c in roman_string:
-        if c is "X":
-            roman_val += 10
-        elif c is "V":
-            roman_val += 5
-        elif c is "I":
-            roman_val += 1
-        elif c is "L":
-            roman_val += 50
-        elif c is "C":
-            roman_val += 100
-        elif c is "D":
-            roman_val += 500
-        elif c is "M":
-            roman_val += 1000
-    return (roman_val)
+    if roman_string:
+        roman_val = 0
+        for c in roman_string:
+            if c is "X":
+                roman_val += 10
+            elif c is "V":
+                roman_val += 5
+            elif c is "I":
+                roman_val += 1
+            elif c is "L":
+                roman_val += 50
+            elif c is "C":
+                roman_val += 100
+            elif c is "D":
+                roman_val += 500
+            elif c is "M":
+                roman_val += 1000
+        return (roman_val)
+    return (0)
