08bd4c04fc462c4cc938513815ed37bc933cd283
peytonbrsmith <peytonbrsmith@gmail.com>
2020-12-17 14:09:51 -0600

    added task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                return(i)
+    new_set = {x for x in set_1 if x in set_2}
+    return (new_set)
