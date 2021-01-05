391951671c62c9eec8a1099730270835d2f52654
peytonbrsmith <peytonbrsmith@gmail.com>
2021-01-05 20:40:01 +0000

    added task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Square Class that holds size
+
+    Attributes:
+        __size (integer): the size of the square instance
+
+    """
+    def __init__(self, new_size=0):
+        """
+        Initializes a square of size new_size
+
+        Args:
+            new_size: the size for the new square instance
+
+        """
+        if isinstance(new_size, int) is True:
+            if new_size >= 0:
+                self.__size = new_size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Returns the area of the base square"""
+        return (self.__size ** 2)
