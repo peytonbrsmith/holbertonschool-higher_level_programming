3c2f6578ed8851eb372ee39cea49d91d733c4aa0
peytonbrsmith <peytonbrsmith@gmail.com>
2021-01-06 15:38:13 +0000

    changed new_position[0] to new_position[1]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -77,10 +77,8 @@ class Square:
         """
         if isinstance(new_position, tuple) is True:
             if len(new_position) is 2:
-                if isinstance(new_position[0], int) is True \
-                        and new_position[0] >= 0:
-                    if isinstance(new_position[1], int) is True \
-                            and new_position[0] >= 0:
+                if isinstance(new_position[0], int) is True and new_position[0] >= 0:
+                    if isinstance(new_position[1], int) is True and new_position[1] >= 0:
                         self.__position = new_position
                         return
         raise TypeError("position must be a tuple of 2 positive integers")
