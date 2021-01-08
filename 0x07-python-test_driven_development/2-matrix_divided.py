586a9e2ee03acf015d19e215c4923147de168fa7
peytonbrsmith <peytonbrsmith@gmail.com>
2021-01-08 12:22:56 -0600

    fixed errors and added test cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,25 +13,29 @@ def matrix_divided(matrix, div):
     Returns:
         a new matrix with the results
     """
+    str = "matrix must be a matrix (list of lists) of integers/floats"
     if type(div) not in list([int, float]):
             raise TypeError("div must be a number")
+    if div is 0:
+        raise ZeroDivisionError("division by zero")
     new_matrix = []
     mtx = []
-    if isinstance(matrix, list) is False:
-        raise \
-            TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if (isinstance(matrix, list) is False):
+        raise TypeError(str)
+    elif not matrix:
+        raise TypeError(str)
+    elif not matrix[0]:
+        raise TypeError(str)
     try:
         columns = len(matrix[0])
     except (TypeError, IndexError):
-        raise \
-            TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(str)
     for row in matrix:
         if len(row) != columns:
             raise TypeError("Each row of the matrix must have the same size")
         for item in row:
             if type(item) not in list([int, float]):
-                raise TypeError\
-                    ("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(str)
             else:
                 mtx.append(round(int(item) / div, 2))
         new_matrix.append(mtx)
