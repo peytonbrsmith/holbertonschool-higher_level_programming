c4aff520cf64198614b0f4c73201a5cf4af5f93a
peytonbrsmith <peytonbrsmith@gmail.com>
2021-01-19 21:51:01 +0000

    added task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Has one method area that is unfinished"""
+    def area(self):
+        """unfinished"""
+        raise Exception("area() is not implemented")
