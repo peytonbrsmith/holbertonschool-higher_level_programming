7fd08902153631d0c4141889f67f0bbfa7fa43c0
peytonbrsmith <peytonbrsmith@gmail.com>
2021-01-20 18:08:14 +0000

    added task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """
-Write a function that writes an Object to a text file, using a JSON representation:
+Write a function that writes an Object to a text file, using a
+JSON representation:
 """
 import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 new_list = []
+old_list = []
 
 for obj in sys.argv:
     if obj is not sys.argv[0]:
         new_list.append(obj)
-print(load_from_json_file("add_item.json"))
-save_to_json_file(new_list, "add_item.json")
-
+try:
+    old_list = load_from_json_file("add_item.json")
+except ValueError:
+    pass
+if new_list:
+    if old_list:
+        new_list += old_list
+    save_to_json_file(new_list, "add_item.json")
