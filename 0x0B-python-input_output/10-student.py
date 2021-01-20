3eb9d7abe58ff6d48d2e41f8a88be77ad46257ef
peytonbrsmith <peytonbrsmith@gmail.com>
2021-01-20 18:56:19 +0000

    added task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student by:"""
+import json
+class_to_json = __import__('8-class_to_json').class_to_json
+
+
+class Student():
+    """
+        Student Class
+
+        --------------
+
+        Public instance attributes:
+            first_name
+            last_name
+            age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves class dictionary and specific keys"""
+        if attrs:
+            new_dict = {}
+            for attr in attrs:
+                if self.__dict__.get(attr):
+                    new_dict[attr] = self.__dict__.get(attr)
+            return new_dict
+        else:
+            return self.__dict__
