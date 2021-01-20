a98ae24ba62b318fc610847abcab771d8e0a1c60
peytonbrsmith <peytonbrsmith@gmail.com>
2021-01-20 08:20:29 +0000

    added task 0 files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """reads a file, prints to stdout"""
+    with open(filename, mode='r', encoding="utf-8") as myFile:
+        print(myFile.read())
