cf6e0e4f0a84e50b4c33f79127e717da7b1006ee
Peyton Smith <peytonbrsmith@gmail.com>
2021-01-20 21:15:59 -0600

    removed import

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,9 +4,8 @@ Write a function that returns the dictionary description with simple data
 structure list, dictionary, string, integer and boolean) for
 JSON serialization of an object:
 """
-import json
 
 
 def class_to_json(obj):
     """Converts class instances into json"""
-    return (json.dumps(obj.__dict__))
+    return obj.__dict__
