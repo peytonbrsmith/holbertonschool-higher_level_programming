832faa61868376ea7944fecfb9d819c9614d49c8
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-02 19:34:51 +0000

    added task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- show mysql user privileges
 -- for two users
-SHOW GRANTS IF EXISTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS IF EXISTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
