58cb666b81eda8dd2e82b1beb2555aed80f36d52
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-22 14:34:14 -0500

    added task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+let x = 0;
+
+process.argv.forEach((val, index) => {
+  if (index === 2) {
+    x = x + 1;
+    console.log(`${val}`);
+  }
+});
+
+if (x === 0) {
+  console.log('No argument');
+}
