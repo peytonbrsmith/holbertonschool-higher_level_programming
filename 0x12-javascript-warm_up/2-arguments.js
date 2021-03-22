d24437401d8c33ce0ccab05dccdd7bbae2b2e2d8
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-22 14:23:24 -0500

    added task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,13 +3,15 @@
 let x = 0;
 
 process.argv.forEach((val, index) => {
-  if (index === 2) {
-    x = 1;
+  if (index > 1) {
+    x = x + 1;
   }
 });
 
 if (x === 1) {
-  console.log('Argument found');
+    console.log('Argument found');
+} else if (x > 1) {
+    console.log('Arguments found');
 } else {
   console.log('No argument');
 }
