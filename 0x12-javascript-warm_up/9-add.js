452faf5bf71e875b79abe39c72e2accf3cb58ab6
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-22 15:54:21 -0500

    added task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+function add (a, b) {
+  return (a + b);
+}
+
+const x = parseInt(process.argv[2]);
+const y = parseInt(process.argv[3]);
+
+if (isNaN(x) || isNaN(y)) {
+  console.log('NaN');
+} else {
+  console.log(add(x, y));
+}
