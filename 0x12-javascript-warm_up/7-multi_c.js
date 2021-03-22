1219211834ec29eb4025c5d8211feaa7aa4ca0f1
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-22 15:25:37 -0500

    added task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+{
+  let x = parseInt(process.argv[2]);
+
+  if (isNaN(x)) {
+    console.log('Missing number of occurences');
+  } else {
+    while (x > 0) {
+      console.log('C is fun');
+      x = x - 1;
+    }
+  }
+}
