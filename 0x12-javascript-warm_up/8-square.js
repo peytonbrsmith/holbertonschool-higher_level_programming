577ec2fcfddebc4a95720d1102fe619d14ae4139
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-22 15:41:43 -0500

    added task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+{
+  let x = parseInt(process.argv[2]);
+  if (isNaN(x)) {
+    console.log('Missing size');
+  } else {
+    const y = x;
+    while (x > 0) {
+      let row = '';
+      for (let i = 0; i < y; i++) {
+        row = row.concat('#');
+      }
+      console.log(row);
+      x = x - 1;
+    }
+  }
+}
