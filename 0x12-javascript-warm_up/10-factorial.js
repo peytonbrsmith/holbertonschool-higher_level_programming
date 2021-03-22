cdcf7d17f0a5f16de3e2e184ba5b961f00437d77
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-22 16:03:26 -0500

    added task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+function fac (num) {
+  if (isNaN(num)) { return 1; }
+  if (num < 0) { return -1; } else if (num == 0) { return 1; } else { return (num * fac(num - 1)); }
+}
+
+const x = parseInt(process.argv[2]);
+
+console.log(fac(x));
