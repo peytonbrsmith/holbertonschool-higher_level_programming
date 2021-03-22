977b5698771e038d92038d9ac8f6e177960b4700
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-22 16:36:28 -0500

    added task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 
 function fac (num) {
   if (isNaN(num)) { return 1; }
-  if (num < 0) { return -1; } else if (num == 0) { return 1; } else { return (num * fac(num - 1)); }
+  if (num < 0) { return -1; } else if (num === 0) { return 1; } else { return (num * fac(num - 1)); }
 }
 
 const x = parseInt(process.argv[2]);
