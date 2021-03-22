dd649c805843fdbe920531d53c6bf47d3239d243
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-22 16:44:29 -0500

    added task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+if (process.argv.length > 3) {
+  const list = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    list.push(process.argv[i]);
+  }
+  list.sort((a, b) => b - a);
+  console.log(list[1]);
+} else if (process.argv.length === 3) {
+  console.log(0);
+} else {
+  console.log(0);
+}
