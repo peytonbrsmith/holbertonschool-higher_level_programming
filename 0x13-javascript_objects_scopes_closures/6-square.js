89e3014bcc5324f3d5723f0a9d2a01753b1bdc0a
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-23 16:37:53 -0500

    adjusted 6's inheritance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
+const OSquare = require('./5-square');
+class Square extends OSquare {
   constructor (size) {
     super(size, size);
   }
