bdbd29c791c1eb98f69e4f084feb377cdb1c8f0c
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-27 18:21:07 +0000

    fixed formatting

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,14 +2,15 @@
 """
 14
 """
+import sys
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from model_city import City
-
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import Session
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
