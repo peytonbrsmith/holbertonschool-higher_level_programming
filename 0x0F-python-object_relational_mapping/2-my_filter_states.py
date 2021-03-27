c275c34ce0f519a4819953f36b412801bb6af49d
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-27 19:10:39 +0000

    replaced states.id with id

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,11 +23,10 @@ if __name__ == '__main__':
 
     cur.execute("SELECT * FROM states \
                 WHERE states.name like '{}'\
-                ORDER BY states.id".format(ARG))
+                ORDER BY id".format(ARG))
     rows = cur.fetchall()
     for row in rows:
-        # if row[1] == ARG:
-            print(row)
+        print(row)
 
     cur.close()
     db.close()
