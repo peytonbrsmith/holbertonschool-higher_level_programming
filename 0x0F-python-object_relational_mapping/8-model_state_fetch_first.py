76e38b03d603d4d3a9eed6cb9258779106500766
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-27 19:36:24 +0000

    removed unneeded :

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,13 +8,13 @@ if __name__ == "__main__":
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import Session
 
-    
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    state = session.query(State).order_by(State.id).first():
+    state = session.query(State).order_by(State.id).first()
     if (state):
         print("{}: {}".format(state.id, state.name))
     else:
