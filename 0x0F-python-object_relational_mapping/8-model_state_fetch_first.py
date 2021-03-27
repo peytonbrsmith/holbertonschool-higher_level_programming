54f354da7ef4e395ae77be140d57e48eb8fbe041
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-27 19:26:51 +0000

    edited formatting

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,13 +2,13 @@
 """
 8-model_state_fetch_first.py
 """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import Session
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
