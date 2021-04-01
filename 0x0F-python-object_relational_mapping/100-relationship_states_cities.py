3694fbaf31a76ea5d13776b5467f0f70a2461cad
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-01 15:35:36 +0000

    added advanced task files

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""100"""
+
+import sqlalchemy
+import sys
+from relationship_city import Base, City
+from relationship_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    uname = sys.argv[1]
+    pword = sys.argv[2]
+    dbname = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           uname, pword, dbname))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sesh = Session()
+
+    add = State(name="California")
+    add.cities = [City(name="San Francisco")]
+
+    sesh.add(add)
+    sesh.commit()
+
+    sesh.close()
+©
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """City Class"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from relationship_state import Base
 
 
 class City(Base):
-    """
-    City Class
-    """
+    """City Class"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id', nullable=False))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
