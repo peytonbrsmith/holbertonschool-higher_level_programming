6255b7228d1d4cef70988669e170167052c1460e
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-01 15:13:21 +0000

    added advanced task

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""City Class"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """
+    City Class
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id', nullable=False))
