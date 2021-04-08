31ca74cca1c1c357f665040aedfd360a2fca055d
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-08 21:33:53 +0000

    update task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends postage
-curl -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
