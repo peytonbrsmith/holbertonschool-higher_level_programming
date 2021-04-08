3599b8bb3c87a1a5278c658c77f1dbbd206e43be
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-08 21:14:30 +0000

    update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # gets allowed http methods using cURL
-ALLOWED=$(curl -sI "$1" | grep "Allow")
-echo "${ALLOWED##*: }"
+ALLOWED=$(curl -sI "$1" | grep "Allow") && echo "${ALLOWED##*: }"
