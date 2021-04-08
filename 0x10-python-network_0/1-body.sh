cd50cb806b455be096db8830a3f0f6bc7d32dac3
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-08 20:37:58 +0000

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # gets the body of status 200 responses
-STATUS=$(curl -sw "%{response_code}" "$1" -o /dev/null)
+STATUS=$(curl -sLw "%{response_code}" "$1" -o /dev/null)
 if [[ "${STATUS}" -eq 200 ]]; then
-    curl -s "$1"
+    curl -sL "$1"
 fi
