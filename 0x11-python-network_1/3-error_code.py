7063a8971e076c0a809350dfd8f540abecc2d712
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 18:07:54 +0000

    added task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+sends request and handles http errors
+"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    url = sys.argv[1]
+
+    req = request.Request(url)
+    try:
+        with request.urlopen(req) as response:
+            this = response.read()
+            print(this.decode("utf-8"))
+    except error.HTTPError as e:
+        error = ''.join((item for item in str(e) if item.isdigit()))
+        print("Error Code:", error)
