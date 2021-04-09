7f99fc31a2a42185def35123c1559e846a64dfc8
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 17:55:51 +0000

    added task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+post request with email
+"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, parse
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    values = {"email": email}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+
+    req = request.Request(url, data=data, method="POST")
+    with request.urlopen(req) as response:
+        this = response.read()
+        print(this.decode("utf-8"))
