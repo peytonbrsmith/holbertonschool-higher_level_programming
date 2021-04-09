f6900b62033e10aa424c16781ee0925dd14fa3aa
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 19:22:37 +0000

    added 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""send request,
+display body,
+handle errors
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code:", req.status_code)
+    else:
+        print(req.text)
