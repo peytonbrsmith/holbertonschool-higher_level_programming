fc52a2224ac22adbce92232206c172f4b6ef06f8
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 20:21:20 +0000

    added 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""get git
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = 'https://api.github.com/user'
+
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    req = requests.get(url, auth=(username, token))
+    print(req.json().get("id"))
