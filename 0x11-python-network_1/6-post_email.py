2888239fbacf4380f2ef7e8c6947b0d15aaa3d7a
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 19:17:19 +0000

    added 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""use requests to display post an email
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {"email": email}
+
+    req = requests.post(url, data=data)
+    print(req.text)
