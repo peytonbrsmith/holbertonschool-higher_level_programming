88d59cc25c4226fab79dd60f4d413a84e9548c8c
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 17:11:53 +0000

    added if name == main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Write a Python script that fetches https://intranet.hbtn.io/status
 """
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    this = response.read()
-    print("Body response:")
-    print("\t- type: ", this.__class__)
-    print("\t- content: ", this)
-    print("\t- utf8 content: ", this.decode("utf-8"))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        this = response.read()
+        print("Body response:")
+        print("\t- type: ", this.__class__)
+        print("\t- content: ", this)
+        print("\t- utf8 content: ", this.decode("utf-8"))
