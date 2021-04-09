aa34a9c81700501d1e21d48ad1c7a67f8ba3701a
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 20:51:44 +0000

    added adv

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""get gitted
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+
+    payload = {
+        'per_page': 10,
+    }
+    req = requests.get(url, params=payload)
+    for commit in req.json():
+        sha = commit.get("sha")
+        author = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(sha, author))
