61a80195cdaaf434d0b6a70f42b56baa307a76da
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 19:37:10 +0000

    added 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""post with json
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = "http://761cd13fb8de.3b3b042d.hbtn-cod.io:5000/search_user"
+
+    qval = ""
+    try:
+        qval = sys.argv[1]
+    except:
+        pass
+
+    data = {"q": qval}
+    req = requests.post(url, data)
+    json = req.json()
+    if (type(json)):
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+    else:
+        print("Not valid JSON")
