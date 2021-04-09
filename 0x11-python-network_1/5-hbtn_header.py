d523fb5330ac2de49c6d9e36e30941481927dd8e
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-09 18:31:30 +0000

    added task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""use requests to display header from url
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    print(req.headers.get("X-Request-Id"))
