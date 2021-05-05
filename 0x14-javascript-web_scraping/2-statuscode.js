e2003ec5a0e16bba3433a3506657c0af41c65814
peytonbrsmith <peytonbrsmith@gmail.com>
2021-05-05 14:47:43 -0500

    added task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+var request = require('request');
+
+host = process.argv[2]
+
+const options = {
+    hostname: 'host',
+    method: 'GET'
+  }
+
+request(host, function(err, res, body) {
+  console.log(res.statusCode)
+});
