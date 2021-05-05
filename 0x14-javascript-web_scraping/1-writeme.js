cefd9867d41e8f4f724f6e3b455cca6600abea6a
peytonbrsmith <peytonbrsmith@gmail.com>
2021-05-05 13:53:42 -0500

    added task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(file, content, (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
